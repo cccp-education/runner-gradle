db5e6a91987c55d6dd5ce1c3695da2641fb84355
cheroliv <cheroliv@users.noreply.github.com>
2026-06-12 04:25:03 +0200

    fix: -PsiteName dynamic (default cheroliv.com) for multi-site bakery pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ plugins {
     id("education.cccp.bakery") version "0.0.1"
 }
 
-bakery { configPath = file("${System.getenv("OFFICE_PATH") ?: throw GradleException("OFFICE_PATH not set")}/sites/cheroliv.com/site.yml").absolutePath }
+val siteName: String = project.findProperty("siteName") as String? ?: "cheroliv.com"
+
+bakery { configPath = file("${System.getenv("OFFICE_PATH") ?: throw GradleException("OFFICE_PATH not set")}/sites/$siteName/site.yml").absolutePath }
 
 tasks.register("publishSite") {
     group = "publish"
-    description = "Bake + deploy cheroliv.com"
+    description = "Bake + deploy $siteName"
     dependsOn("bake", "deploySite")
 }
